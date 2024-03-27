a4836352f715e80379dbc0147635fe9035af4b97
toondebooser <toondebooser@gmail.com>
2024-03-27 20:26:30 +0100

    Add dynamic user input functionality for the contact page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-contact',
+  selector: "app-contact",
   standalone: true,
   imports: [],
-  templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  templateUrl: "./contact.component.html",
+  styleUrl: "./contact.component.css",
 })
 export class ContactComponent {
-
+  contactData: any = {
+    to: "1",
+    from: "2",
+    phone: "3",
+    subject: "4",
+    message: "5",
+  };
+  getObjectKeys(obj: any): string[] {
+    return Object.keys(obj);
+  }
+  userInput(event: Event, dataType: string): void {
+    const input = event.target as HTMLInputElement;
+    this.contactData[dataType] = input.value;
+    console.log(this.contactData);
+  }
 }
