6ccffea40414e07e970b8a927529a339f62aedae
toondebooser <toondebooser@gmail.com>
2024-03-27 20:39:23 +0100

    Add simple validation for an email adres in contactComponent

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -9,18 +9,24 @@ import { Component } from "@angular/core";
 })
 export class ContactComponent {
   contactData: any = {
-    to: "1",
-    from: "2",
-    phone: "3",
-    subject: "4",
-    message: "5",
+    to: "",
+    from: "",
+    phone: "",
+    subject: "",
+    message: "",
   };
   getObjectKeys(obj: any): string[] {
     return Object.keys(obj);
+  }
+  isValidEmail(input: string): boolean {
+    // Regular expression pattern for email validation
+    const emailPattern: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(input);
   }
   userInput(event: Event, dataType: string): void {
     const input = event.target as HTMLInputElement;
     this.contactData[dataType] = input.value;
     console.log(this.contactData);
+    console.log(this.isValidEmail(input.value))
   }
 }
