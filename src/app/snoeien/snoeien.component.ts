4207a9dc8a71741ea45a59413a295884e621afa2
toondebooser <toondebooser@gmail.com>
2024-04-02 18:41:41 +0200

    Add content for Atelier page

--- src/app/snoeien/snoeien.component.ts
+++ src/app/snoeien/snoeien.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
 
 @Component({
-  selector: 'app-snoeien',
+  selector: "app-snoeien",
   standalone: true,
-  imports: [],
-  templateUrl: './snoeien.component.html',
-  styleUrl: './snoeien.component.css'
+  imports: [RouterLink],
+  templateUrl: "./snoeien.component.html",
+  styleUrl: "./snoeien.component.css",
 })
-export class SnoeienComponent {
-
-}
+export class SnoeienComponent {}
